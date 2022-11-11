2a9b24a3831354f031cee7d928ca138aedfeb274
IshworMoktan <89696918+IshworMoktan@users.noreply.github.com>
2022-11-11 15:48:50 +0545

    some changes

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'data_service.dart';
 import 'models.dart';
-
-
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -10,58 +9,81 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-
   final _cityTextController = TextEditingController();
   final _dataService = DataService();
 
-    WeatherResponse? _response;
+  void saveText(String text) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("text", text);
+  }
+
+  void readText() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? savedValue = prefs.getString("text");
+    if (savedValue != null) {
+      _cityTextController.text =
+          savedValue; //we used sharedpreference to show last typed city's name
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    readText();
+  }
+
+  WeatherResponse? _response;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-          body: Center(
-            child: Container(
-              width: 350,
-              height: 500,
-              decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.blue,
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(150))
-              ),
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            width: 350,
+            height: 500,
+            decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.blue,
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(150))),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (_response != null)
+                  Column(
+                    children: [
 
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  if (_response != null)
-                    Column(
-                      children: [
-                        Image.network(_response!.iconUrl),
-                        Text(
-                          '${(((_response!.tempInfo.temperature)-32)*(5/9)).toStringAsFixed(1)}°c',
-                          style: TextStyle(fontSize: 40),
-                        ),
-                        Text(_response!.weatherInfo.description),
-                      ],
-                    ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 50),
-                    child: SizedBox(
-                      width: 150,
-                      child: TextField(
-                          controller: _cityTextController,
-                          decoration: InputDecoration(labelText: 'Enter your Location',),
-                          textAlign: TextAlign.center),
-                    ),
+                      Image.network(_response!.iconUrl),
+                      Text(
+                        '${(((_response!.tempInfo.temperature) - 32) * (5 / 9)).toStringAsFixed(1)}°c',
+                        style: TextStyle(fontSize: 40),
+                      ),
+                      Text(_response!.weatherInfo.description),
+                    ],
                   ),
-                  ElevatedButton(onPressed: _search, child: Text('Search'))
-                ],
-              ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 50),
+                  child: SizedBox(
+                    width: 150,
+                    child: TextField(
+                        onChanged: (val) {
+                          saveText(val);
+                        },
+                        controller: _cityTextController,
+                        decoration: InputDecoration(
+                          labelText: 'Enter your Location',
+                        ),
+                        textAlign: TextAlign.center),
+                  ),
+                ),
+                ElevatedButton(onPressed: _search, child: Text('Search'))
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 
   void _search() async {
@@ -69,5 +91,3 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() => _response = response);
   }
 }
-
-
